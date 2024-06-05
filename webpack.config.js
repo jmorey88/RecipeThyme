781d45da565d9a954d62932a96a7411be2a9e1a9
jmorey88 <joelmorey@joels-MacBook-Pro.local>
2024-06-05 08:46:12 -0700

    changed webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,96 @@
 // webpack.config.js
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  context: __dirname,
-  entry: "./frontend/recipe_thyme.jsx",
-  output: {
-    path: path.resolve(__dirname, "app", "javascript"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-      {
-        test: /\.module\.css$/, // Targets only CSS files ending with .module.css
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
+  return {
+    mode: isProduction ? "production" : "development",
+    context: __dirname,
+    entry: "./frontend/recipe_thyme.jsx",
+    output: {
+      path: path.resolve(__dirname, "public", "assets"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              modules: {
-                mode: "local",
-                localIdentName: "[name]__[local]___[hash:base64:5]", // Configures the generated class names
-              },
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
           },
-        ],
-      },
-    ],
-  },
-  // devtool: "eval-source-map",
-  // resolve: {
-  //   extensions: [".js", ".jsx"],
-  // },
+        },
+        {
+          test: /\.module\.css$/, // Targets only CSS files ending with .module.css
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  mode: "local",
+                  localIdentName: "[name]__[local]___[hash:base64:5]", // Configures the generated class names
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+  };
 };
+
+// webpack.config.js
+// const path = require("path");
+
+// module.exports = {
+//   mode: "development",
+//   context: __dirname,
+//   entry: "./frontend/recipe_thyme.jsx",
+//   output: {
+//     path: path.resolve(__dirname, "app", "javascript"),
+//     filename: "bundle.js",
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env", "@babel/preset-react"],
+//           },
+//         },
+//       },
+
+//       {
+//         test: /\.module\.css$/, // Targets only CSS files ending with .module.css
+//         use: [
+//           "style-loader",
+//           {
+//             loader: "css-loader",
+//             options: {
+//               modules: {
+//                 mode: "local",
+//                 localIdentName: "[name]__[local]___[hash:base64:5]", // Configures the generated class names
+//               },
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// devtool: "eval-source-map",
+// resolve: {
+//   extensions: [".js", ".jsx"],
+// },
+// };
