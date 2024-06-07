0d809f83598e416fcafa265f9684100fe421387e
jmorey88 <joelmorey@joels-MBP-16.lan>
2024-06-07 00:15:10 -0700

    checking heroku build

--- frontend/features/auth/sessionService.js
+++ frontend/features/auth/sessionService.js
@@ -36,7 +36,7 @@ export const postLogin = async (userCredentials) => {
   if (process.env.NODE_ENV === "production") {
     console.log(
       process.env.NODE_ENV,
-      "<<<<<<<<<<<<<<<<<production!!!>>>>>>>>>>>>>>>"
+      "<<<<<<<<<<<<<<<<<<production!!!>>>>>>>>>>>>>>>"
     );
   } else {
     console.log(process.env.NODE_ENV, "<<<<<<<dev?>>>>>>>>>");
