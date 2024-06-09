eb73e8ddd69c82f31837da36e056bfac7151870a
jmorey88 <joelmorey@joels-MBP-5.lan>
2024-06-08 22:57:21 -0700

    changed webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify(environment),
+        NODE_ENV: JSON.stringify(environment),
       }),
     ],
     module: {
