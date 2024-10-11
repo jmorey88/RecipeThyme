09bac9bd2a89fc55dc2e5dd9d6e0434114595748
jmorey88 <joel.morey88@gmail.com>
2024-10-11 07:07:24 -0700

    added back lazy loading

--- frontend/App.js
+++ frontend/App.js
@@ -2,27 +2,27 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./features/home/index";
 import SessionsForm from "./features/sessions/index";
-import RecipeGallery from "./features/recipes/RecipeGallery/index.js";
+// import RecipeGallery from "./features/recipes/RecipeGallery/index.js";
 import NavBar from "./features/navBar/index";
-import RecipeDetail from "./features/recipes/RecipeDetail/index.js";
-import RecipeForm from "./features/recipes/RecipeCreate/index.js";
+// import RecipeDetail from "./features/recipes/RecipeDetail/index.js";
+// import RecipeForm from "./features/recipes/RecipeCreate/index.js";
 import { AuthRoute, ProtectedRoute } from "./utils/routes_util.jsx";
-import EditRecipeForm from "./features/recipes/RecipeEdit/index.js";
+// import EditRecipeForm from "./features/recipes/RecipeEdit/index.js";
 import LayoutWithFooter from "./features/footer/LayoutWithFooter.js";
 import ScrollToTop from "./utils/scrollToTopUtil.js";
 
-// const RecipeDetail = lazy(() =>
-//   import("./features/recipes/RecipeDetail/index.js")
-// );
-// const RecipeGallery = lazy(() =>
-//   import("./features/recipes/RecipeGallery/index.js")
-// );
-// const RecipeForm = lazy(() =>
-//   import("./features/recipes/RecipeCreate/index.js")
-// );
-// const EditRecipeForm = lazy(() =>
-//   import("./features/recipes/RecipeEdit/index.js")
-// );
+const RecipeDetail = lazy(() =>
+  import("./features/recipes/RecipeDetail/index.js")
+);
+const RecipeGallery = lazy(() =>
+  import("./features/recipes/RecipeGallery/index.js")
+);
+const RecipeForm = lazy(() =>
+  import("./features/recipes/RecipeCreate/index.js")
+);
+const EditRecipeForm = lazy(() =>
+  import("./features/recipes/RecipeEdit/index.js")
+);
 
 function App() {
   return (
@@ -59,9 +59,9 @@ function App() {
           element={
             <ProtectedRoute>
               <LayoutWithFooter>
-                {/* <Suspense fallback={<div>Loading...</div>}> */}
-                <RecipeGallery />
-                {/* </Suspense> */}
+                <Suspense fallback={<div>Loading...</div>}>
+                  <RecipeGallery />
+                </Suspense>
               </LayoutWithFooter>
             </ProtectedRoute>
           }
@@ -71,9 +71,9 @@ function App() {
           element={
             <ProtectedRoute>
               <LayoutWithFooter>
-                {/* <Suspense fallback={<div>Loading...</div>}> */}
-                <RecipeDetail />
-                {/* </Suspense> */}
+                <Suspense fallback={<div>Loading...</div>}>
+                  <RecipeDetail />
+                </Suspense>
               </LayoutWithFooter>
             </ProtectedRoute>
           }
@@ -82,9 +82,9 @@ function App() {
           path="/recipe-create"
           element={
             <LayoutWithFooter>
-              {/* <Suspense fallback={<div>Loading...</div>}> */}
-              <RecipeForm />
-              {/* </Suspense> */}
+              <Suspense fallback={<div>Loading...</div>}>
+                <RecipeForm />
+              </Suspense>
             </LayoutWithFooter>
           }
         />
@@ -92,9 +92,9 @@ function App() {
           path="/recipe/:recipeId/edit"
           element={
             <LayoutWithFooter>
-              {/* <Suspense fallback={<div>Loading...</div>}> */}
-              <EditRecipeForm />
-              {/* </Suspense> */}
+              <Suspense fallback={<div>Loading...</div>}>
+                <EditRecipeForm />
+              </Suspense>
             </LayoutWithFooter>
           }
         />
